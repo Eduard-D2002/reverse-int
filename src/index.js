5b541fb2fca7a69ff16f6d7ef3aa99f27d42e2d0
Eduard-D2002 <edomalchuk@gmail.com>
2020-10-05 10:19:19 +0300

    feat:implement reverseInt func

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function reverse (n) {
-  
+   n = n.toString();
+   if(n.slice(0,1) === '-'){
+     n = n.slice(1);
+   }
+   return (
+     parseFloat(
+       n
+         .toString()
+         .split('')
+         .reverse()
+         .join('')
+     ) * Math.sign(n)
+   )  
 }
